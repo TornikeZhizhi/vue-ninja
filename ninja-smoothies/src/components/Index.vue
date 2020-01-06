7e611d2d43c9e084b4a27ea4dea5717c2da51ecd
tornikezhizhi <zhizhiashvilitornike@gmail.com>
2020-01-06 15:22:55 +0400

    add starting files, style index

---
+++ ninja-smoothies/src/components/Index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="index container">
+    <div class="card" v-for="(smoothie, index) in smoothies" :key="smoothie.id">
+      <div class="card-content">
+        <h4 class="indigo-te">{{ smoothie.title }}</h4>
+      </div>
+      <ul class="ingridients">
+        <li v-for="(ing, index) in smoothie.ingredients" :key="index">
+          <span class="chip">{{ ing }}</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {
+      smoothies: [
+        {
+          title: "Ninja Brew",
+          slug: "ninja-bew",
+          ingredients: ["banans", "coffee", "milk"],
+          id: "1"
+        },
+        {
+          title: "Morning Mood",
+          slug: "morning-mood",
+          ingredients: ["mandgo", "lime", "juce"],
+          id: "2"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.ingridients li {
+  display: inline;
+}
+.index {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 30px;
+  margin-top: 20px;
+}
+.index h4 {
+  font-size: 1.8rem;
+  text-align: center;
+}
+.card-content {
+  padding: 10px 10px 5px 24px;
+}
+.ingridients {
+  padding: 0 0 20px 20px;
+}
+</style>
---
+++ ninja-smoothies/src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="navbar">
+    <nav class="nav-extended inidgo darken-2">
+      <div class="nav-content">
+        <router-link to="">
+          <span class="nav-title">Ninja Smoothies</span>
+        </router-link>
+        <a href="" class="btn-floating btn-large halfway-fab pink">
+          <router-lnk to="">
+            <i class="material-icons">add</i>
+          </router-lnk>
+        </a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+.nav-extended {
+  padding: 0 20px;
+}
+</style>
